60bd36907caa50e5e88ebfa099732d803144c17f
Thomas Mol <email@thomasmol.com>
2022-09-05 13:45:31 +0200

    tailwind and config

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+    container: {
+			center: true,
+			padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '3rem',
+        xl: '4rem',
+        '2xl': '4rem',
+      },
+      screens: {
+				sm: "640px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1280px",
+				'2xl': "1280px"
+		 }
+    },
+  },
+  plugins: [
+		require('@tailwindcss/forms'),
+	]
+}
